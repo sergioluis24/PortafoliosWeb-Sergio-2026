7eba43e3db85a4c13427d42ca13fd719b43b5f08
sergioluis24 <sergioluisl2324@gmail.com>
2026-04-14 07:10:11 -0400

    Feat:add adapter for routing by demands

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,21 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   integrations: [react()],
+
+  adapter: node({
+    mode: "middleware",
+  }),
+
+  // adapter: node({
+  //   mode: "standalone",
+  // }),
 });
